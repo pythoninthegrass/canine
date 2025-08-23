034eb8cc8a0b55d82ae00de90749bafd5c1ef5cf
Chris <chris@example.com>
2025-08-23 13:40:10 -0700

    build cloud provider credential

--- app/models/build_configuration.rb
+++ app/models/build_configuration.rb
@@ -8,16 +8,19 @@
 #  updated_at     :datetime         not null
 #  build_cloud_id :bigint
 #  project_id     :bigint           not null
+#  provider_id    :bigint           not null
 #
 # Indexes
 #
 #  index_build_configurations_on_build_cloud_id  (build_cloud_id)
 #  index_build_configurations_on_project_id      (project_id)
+#  index_build_configurations_on_provider_id     (provider_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (build_cloud_id => build_clouds.id)
 #  fk_rails_...  (project_id => projects.id)
+#  fk_rails_...  (provider_id => providers.id)
 #
 class BuildConfiguration < ApplicationRecord
   belongs_to :project
