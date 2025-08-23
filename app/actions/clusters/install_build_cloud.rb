1ab555d2971538cfb4208bbc379a3c5f7dc46ac2
Chris <chris@example.com>
2025-08-23 12:05:13 -0700

    fix authentication error with build clouds

--- app/actions/clusters/install_build_cloud.rb
+++ app/actions/clusters/install_build_cloud.rb
@@ -10,15 +10,6 @@ module Clusters
     executed do |context|
       build_cloud = context.build_cloud
       # Check if build cloud is already installed
-      if build_cloud.pending? || build_cloud.failed?
-        build_cloud.installing!
-      else
-        build_cloud.updating!
-      end
-      if build_cloud.uninstalled?
-        build_cloud.update(error_message: nil, status: :installing)
-      end
-
       # Create BuildCloud record (namespace will use default from migration)
       build_cloud = K8::BuildCloudManager.install(build_cloud)
       context.build_cloud = build_cloud
