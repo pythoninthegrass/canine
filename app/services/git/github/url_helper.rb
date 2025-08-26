4d5b905346617112dfb441b844daeef982969d03
Celina Lopez <marisa.celina.lopez4@gmail.com>
2025-08-26 15:18:09 -0700

    update url helper

--- app/services/git/github/url_helper.rb
+++ app/services/git/github/url_helper.rb
@@ -1,13 +1,16 @@
 class Git::Github::UrlHelper
+  HOST = Rails.application.routes.default_url_options[:host]
+
   def self.authorize_url
-    base_url = "https://github.com/login/oauth/authorize"
+    redirect_uri = Rails.application.routes.url_helpers.github_oauth_url(host: HOST, protocol: "https")
     params = {
       response_type: "code",
       client_id: ENV["OMNIAUTH_GITHUB_PUBLIC_KEY"],
-      redirect_uri: Rails.application.routes.url_helpers.github_oauth_url(host: Rails.application.routes.default_url_options[:host]),
-      scope: "id email name",
+      scope: "id emailname",
       state: SecureRandom.uuid
     }
-    "#{base_url}?#{params.to_query}"
+    uri = URI::HTTPS.build(host: "github.com", path: "/login/oauth/authorize", query: params.to_query)
+    uri.query += "&redirect_uri=#{redirect_uri}"
+    uri.to_s
   end
 end
