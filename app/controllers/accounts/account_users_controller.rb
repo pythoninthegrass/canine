669be2d3e481209017a827fc2591e21f7a222cda
Chris <chris@example.com>
2025-09-10 11:26:12 -0700

    remove account setting

--- app/controllers/accounts/account_users_controller.rb
+++ app/controllers/accounts/account_users_controller.rb
@@ -1,24 +1,23 @@
 class Accounts::AccountUsersController < ApplicationController
-  before_action :set_account
   def create
     user = User.find_or_initialize_by(email: user_params[:email]) do |user|
       user.first_name = user_params[:email].split("@").first
       user.password = Devise.friendly_token[0, 20]
       user.save!
     end
-    AccountUser.create!(account: @account, user: user)
+    AccountUser.create!(account: current_account, user: user)
 
-    redirect_to account_account_users_path(@account), notice: "User was successfully added."
+    redirect_to account_account_users_path(current_account), notice: "User was successfully added."
   end
 
   def destroy
-    @account.account_users.find(params[:id]).destroy
+    current_account.account_users.find(params[:id]).destroy
 
-    redirect_to account_account_users_path(@account), notice: "User was successfully destroyed."
+    redirect_to account_account_users_path(current_account), notice: "User was successfully destroyed."
   end
 
   def index
-    @pagy, @account_users = pagy(@account.account_users)
+    @pagy, @account_users = pagy(current_account.account_users)
   end
 
 
@@ -26,9 +25,5 @@ class Accounts::AccountUsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email)
-  end
-
-  def set_account
-    @account = Account.find(params[:account_id])
   end
 end
