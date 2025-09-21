e27ff07cda0d79f1c3818b5c188ccd42bd878b44
Chris <chris@example.com>
2025-09-21 00:26:11 -0400

    fix account switching

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,6 @@
 class AccountsController < ApplicationController
   def switch
-    @account = current_user.accounts.find(params[:id])
+    @account = current_user.accounts.friendly.find(params[:id])
     session[:account_id] = @account.id
     redirect_to root_path
   end
