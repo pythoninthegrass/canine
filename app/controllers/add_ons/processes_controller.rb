6f0ab4cc510318449a4f9c2f74b2231c1717acff
Chris <chris@example.com>
2025-10-26 00:07:57 -0700

    dont 500 on pod pending logs

--- app/controllers/add_ons/processes_controller.rb
+++ app/controllers/add_ons/processes_controller.rb
@@ -5,8 +5,10 @@ class AddOns::ProcessesController < AddOns::BaseController
 
   def show
     client = K8::Client.new(active_connection)
-    @logs = client.get_pod_log(params[:id], @add_on.name)
     @pod_events = client.get_pod_events(params[:id], @add_on.name)
+    @logs = client.get_pod_log(params[:id], @add_on.name)
+  rescue Kubeclient::HttpError => e
+    @logs = e.to_s
   rescue Kubeclient::ResourceNotFoundError
     flash[:alert] = "Pod #{params[:id]} not found"
     redirect_to add_on_processes_path(@add_on)
