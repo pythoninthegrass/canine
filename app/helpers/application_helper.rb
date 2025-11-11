ca78fbbfd47b6b4fdc97015c14e59e1c90b7ba04
Chris <chris@example.com>
2025-11-10 19:11:25 -0500

    pagination fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,14 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def custom_pagy_nav(pagy)
-    html = '<div class="join">'
+    html = '<div class="flex items-center gap-2">'
     html << pagy_nav_prev(pagy) if pagy.prev
     pagy.series.each do |item|
-      html << link_to(item, url_for(page: item), class: "btn btn-primary join-item btn-active btn-sm")
+      if item.to_i == pagy.page.to_i
+        html << link_to(item, url_for(page: item), class: "btn btn-sm btn-neutral min-w-[2.5rem] pointer-events-none")
+      else
+        html << link_to(item, url_for(page: item), class: "btn btn-sm btn-ghost min-w-[2.5rem]")
+      end
     end
     html << pagy_nav_next(pagy) if pagy.next
     html << "</div>"
@@ -14,15 +18,21 @@ module ApplicationHelper
   end
 
   def pagy_nav_prev(pagy)
-    '<a aria-label="pagination-prev" class="btn join-item btn-sm gap-2" href="' + pagy_url_for(pagy, pagy.prev) + '">' +
+    '<a aria-label="pagination-prev" class="btn btn-sm gap-2" href="' + pagy_url_for(pagy, pagy.prev) + '">' +
     '<iconify-icon icon="lucide:chevron-left" height="16"></iconify-icon>' +
     "</a>"
   end
 
   def pagy_nav_next(pagy)
-    '<a aria-label="pagination-prev" class="btn join-item btn-sm gap-2" href="' + pagy_url_for(pagy, pagy.next) + '">' +
+    '<a aria-label="pagination-prev" class="btn btn-sm gap-2" href="' + pagy_url_for(pagy, pagy.next) + '">' +
     '<iconify-icon icon="lucide:chevron-right" height="16"></iconify-icon>' +
     "</a>"
+  end
+
+  def pagy_info_text(pagy)
+    from = ((pagy.page - 1) * pagy.limit) + 1
+    to = [ pagy.page * pagy.limit, pagy.count ].min
+    "Showing #{from}-#{to} of #{pagy.count}"
   end
 
   def in_namespace?(namespace)
