43237df2b87de9e2d72d543d15df38cb0408e7a1
Chris <chris@example.com>
2025-11-12 01:01:03 -0500

    update tokens

--- db/migrate/20251022183720_create_api_tokens.rb
+++ db/migrate/20251022183720_create_api_tokens.rb
@@ -2,10 +2,12 @@ class CreateApiTokens < ActiveRecord::Migration[7.2]
   def change
     create_table :api_tokens do |t|
       t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
       t.string :access_token, null: false
       t.datetime :last_used_at
       t.datetime :expires_at
       t.timestamps
     end
+    add_index :api_tokens, :access_token, unique: true 
   end
 end
