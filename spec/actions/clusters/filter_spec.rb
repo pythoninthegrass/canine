af601a3c2764d11fd27390c03360341085ae6b0b
Chris <chris@example.com>
2025-12-12 14:56:00 -0800

    added spec coverage

---
+++ spec/actions/clusters/filter_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Clusters::Filter do
+  it 'filters clusters by name with case-insensitive ILIKE' do
+    account = create(:account)
+    prod = create(:cluster, account: account, name: 'production-us')
+    create(:cluster, account: account, name: 'staging-eu')
+
+    expect(described_class.execute(params: { q: 'PROD' }, clusters: Cluster.all).clusters).to eq([prod])
+    expect(described_class.execute(params: { q: '' }, clusters: Cluster.all).clusters.count).to eq(2)
+  end
+end
