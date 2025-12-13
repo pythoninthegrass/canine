3a61b10e8112f1d56bcce9ebab3e4fa2ba28c379
Chris <chris@example.com>
2025-12-13 15:50:01 -0800

    make forms better for creating a new project

--- app/javascript/controllers/radio_selector_controller.js
+++ app/javascript/controllers/radio_selector_controller.js
@@ -4,17 +4,19 @@ export default class extends Controller {
   static targets = ["radio", "partial"]
   
   connect() {
-    this.toggle()
+    //this.toggle()
   }
 
   toggle() {
     const selectedRadio = this.radioTargets.find(radio => radio.checked)
+    console.log(selectedRadio)
     
     if (selectedRadio) {
       const selectedValue = selectedRadio.value
       
       this.partialTargets.forEach(partial => {
         if (partial.dataset.value === selectedValue) {
+          console.log(partial)
           partial.classList.remove('hidden')
         } else {
           partial.classList.add('hidden')
