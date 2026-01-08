9f2a71628eb089abf531da9eea534692297fb4ad
Chris <chris@example.com>
2026-01-08 03:29:35 -0800

    updated deployment configuration migration position

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_06_190845) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_08_112341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
